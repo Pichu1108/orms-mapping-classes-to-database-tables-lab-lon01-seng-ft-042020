7fcaaf93442300fd992a85476a87f3e3c20e77ab
Manuel Alb Martin jukisz <manujukisz@outlook.com>
2020-05-02 17:27:41 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -8,4 +8,17 @@ attr_accessor :name, :grade
     @grade = grade
   end
 
+
+  def self.create_table
+    sql =  <<-SQL
+      CREATE TABLE IF NOT EXISTS songs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        album TEXT
+      )
+      SQL
+    DB[:conn].execute(sql)
+
+  end
+
 end
